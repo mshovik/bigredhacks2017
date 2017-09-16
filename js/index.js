8fb3e8474f0fae830b51d6fe3e515d5f83d4de8c
Mahfuza Shovik <mahfuzashovik@dhcp-ccc-1278.eduroam.cornell.edu>
2017-09-16 13:56:26 -0400

    started front end

---
+++ js/index.js
@@ -0,0 +1,3 @@
+var students = ['Labiba Chowdhury', 'Mahfuza Shovik', 'Mishcat Ibrahim']
+var student = students[Math.floor(Math.random()*students.length)]
+document.body.innerHTML = "<h1>Welcome, " + student + "</h1>"
