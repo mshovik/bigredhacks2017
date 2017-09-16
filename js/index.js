49cc671f9928b415a8d339dd2ca57131a0884ba7
Mahfuza Shovik <mahfuzashovik@dhcp-ccc-1278.eduroam.cornell.edu>
2017-09-16 14:43:43 -0400

    created welcome message

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
-var students = ['Labiba Chowdhury', 'Mahfuza Shovik', 'Mishcat Ibrahim']
-var student = students[Math.floor(Math.random()*students.length)]
-document.body.innerHTML = "<h1>Welcome, " + student + "</h1>"
+function welcomeMessage() {
+  var students = ['Labiba Chowdhury', 'Mahfuza Shovik', 'Mishcat Ibrahim']
+  var student = students[Math.floor(Math.random()*students.length)]
+  document.body.innerHTML = "<h1>Welcome, " + student + "</h1>"
+}
